66a8fe46974eb210076d865340f94f6f426036d4
Peter Reuterås <peter@reuteras.com>
2025-03-17 16:01:00 +0100

    Add renderable

--- src/rwreader/ui/screens/link_selection.py
+++
@@ -1,6 +0,0 @@
-"""Link selection screen for rwreader."""
-
-# Re-export LinkSelectionScreen from the module
-from ..widgets.link_selection_screen import LinkSelectionScreen
-
-__all__: list[str] = ["LinkSelectionScreen"]
